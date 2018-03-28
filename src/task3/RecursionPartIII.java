4cfbd81b0b492682598c02022004eb769bc646b7
abstructs <andrewclarke540@gmail.com>
2018-03-28 14:18:10 -0400

    create stubs for task3

---
+++ src/task3/RecursionPartIII.java
@@ -0,0 +1,44 @@
+package task3;
+
+public class RecursionPartIII {
+    // Precondition: x is less than or equal to y
+    // Postcondition: This method prints a pattern of 2*(y-x+1) lines
+    // to the stdout (console). The first line contains x asterisks, the next
+    // line contains x+1 asterisks, and so on up to a line with y asterisks.
+    // Then the pattern is then reversed backwards, going y back down to x.
+      /* Example output:
+         pattern(3, 5) will output:
+         ***
+         ****
+         *****
+         *****
+         ****
+         ***
+    */
+    public static void pattern(int x, int y) {
+        return;
+    }
+
+    // Precondition: none
+    // Postcondition: This method outputs a string (title) followed by numbers
+    // of the form 1.1, 1.2, 1.3 etc... The levels input parameter, determines how // many levels the section numbers have. To clarify, an input of levels equal // to 2, would have section number of the form x.y. Likewise, if levels is
+    // equal to 3, would have section number of the form x.y.z. Please note the // digits permitted in each level are only 1 – 9.
+    /* Example output:
+       level(“TITLE”, 2) will output:
+       TITLE1.1.
+       TITLE1.2.
+       TITLE1.3.
+    //and end by printing
+       TITLE9.7.
+       TITLE9.8.
+       TITLE9.9.
+    */
+
+    public static void level(String title, int levels){
+        return;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
