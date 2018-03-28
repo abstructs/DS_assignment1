7e5507ec7488432ca5028486e2b29f79728ef98c
abstructs <andrewclarke540@gmail.com>
2018-03-28 12:43:18 -0400

    do a & b

--- src/Task1/TestTiming.java
+++ src/Task1/TestTiming.java
@@ -1,18 +1,39 @@
 package Task1;
 
 public class TestTiming {
-    public static void main(String args[]) {
+    public static Double testDaffy(int n) {
         Stopwatch timer;
         Timing t;
         timer = new Stopwatch();
         //initialize arguments
         timer.start();
         t = new Timing();
-
         //code to be timed
-        t.daffy(10);
-
+        t.daffy(n);
         timer.stop();
-        System.out.println(timer);
+        return Double.parseDouble(timer.toString());
+    }
+
+    public static Double testDonald(int n) {
+        Stopwatch timer;
+        Timing t;
+        timer = new Stopwatch();
+        //initialize arguments
+        timer.start();
+        t = new Timing();
+        //code to be timed
+        t.donald(n);
+        timer.stop();
+        return Double.parseDouble(timer.toString());
+    }
+
+    public static void main(String args[]) {
+        for(int i = 30; i <= 44; i++) {
+            System.out.println(testDaffy(i).toString());
+        }
+
+        for(int i = 30; i <= 44; i++) {
+            System.out.println(testDonald(i).toString());
+        }
     }
 }
