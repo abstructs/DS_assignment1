b9bab35dce8b9f4b2b244b89c6aceda3bf5a442d
abstructs <andrewclarke540@gmail.com>
2018-03-28 12:23:17 -0400

    add task1 package

--- src/Task1/Stopwatch.java
+++ src/Task1/Stopwatch.java
@@ -1,3 +1,5 @@
+package Task1;
+
 /**
  A class to measure time elapsed.
 */
--- src/Task1/TestTiming.java
+++ src/Task1/TestTiming.java
@@ -1,11 +1,17 @@
+package Task1;
+
 public class TestTiming {
     public static void main(String args[]) {
-        Stopwatch timer = new Stopwatch();
+        Stopwatch timer;
+        Timing t;
+        timer = new Stopwatch();
         //initialize arguments
         timer.start();
-        Timing t = new Timing();
-        t.daffy(10);
+        t = new Timing();
+
         //code to be timed
+        t.daffy(10);
+
         timer.stop();
         System.out.println(timer);
     }
