82eac740fbee46e748201fc5310c9cd35f749ad3
Dindyal Mursingh <dindyal.mursingh@gmail.com>
2018-03-30 12:24:27 -0400

    still working
    still adapting methods.
    -doing basic functionality
    -will do a full implement of all the methods at the end

--- src/task2/Driver.java
+++ src/task2/Driver.java
@@ -5,18 +5,22 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LinkedSequence<String> L = new LinkedSequence<String>();
+		System.out.println("Original List L: " + L.getCurrent());
 		L.addAfter("2");
 		L.addAfter("3");
 		L.addAfter("4");
+		
 		LinkedSequence<String> L2 = new LinkedSequence<String>();
 		
 		L2.addAfter("5");
 		L2.addAfter("6");
 		L2.addAfter("7");
 		
-		L.addAll(L2);
+		//L.addAll(L2);
+		L.addBefore("3.5");
 		
-		System.out.println("Original List L: " + L.toString());
+		
+		System.out.println("Original List L: " + L.getCurrent());
 	}
 
 }
